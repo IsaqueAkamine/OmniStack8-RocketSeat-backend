a7ed5ad80d66ff30fb05a330aeec89b560ca578e
Isaque Akamine <isaqueakamine@gmail.com>
2020-02-03 22:26:13 -0300

    add axios and get user data from github api

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const routes = require('./routes');
 
 const server = express();
 
+// Used mongodb.atlas (cloud database)
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-lhv8m.mongodb.net/omnistack8?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
